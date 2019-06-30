ae38548248618d849ba9bcab291a59c4d8344175
cxmshine <13724591964@163.com>
2019-06-30 13:11:19 +0800

    完成高级-02的学习

--- src/main/java/com/nowcoder/WendaApplication.java
+++ src/main/java/com/nowcoder/WendaApplication.java
@@ -1,4 +1,4 @@
-package com.nowcoder.wenda;
+package com.nowcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WendaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WendaApplication.class, args);
+	public static void main(String[] args){
+	 	SpringApplication.run(WendaApplication.class,args);
 	}
 
 }
